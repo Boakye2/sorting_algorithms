2809173f9435aec2238d12ee9dfcac4f458a8079
Boakye2 <nanaboakyefred2@gmail.com>
2022-07-25 14:44:01 +0200

    4. Shell sort - Knuth Sequence

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,50 +1,34 @@
 #include "sort.h"
-#Boakye
 
 /**
- * swap_int - swaps the values of two integers
- * @a: integer a
- * @b: integer b
- */
-void swap_int(int *a, int *b)
-{
-	int a1;
-
-	a1 = *a;
-	*a = *b;
-	*b = a1;
-}
-
-/**
- * shell_sort - sorts an array of integers in ascending order
- * @array: pointer to an array of integers
- * @size: the number of elements in the array
+ * shell_sort - sorts an array of integers in ascending order using the
+ * Shell sort algorithm
+ * @array: array to sort
+ * @size: size of the array to sort
+ *
+ * Return: void
  */
 void shell_sort(int *array, size_t size)
 {
+	size_t i, j, gap = 1;
+	int insert;
 
-	size_t i, n, interval = 1;
+	if (array == NULL || size < 2)
+		return;
 
-	while (interval < size / 3)
-	{ /* This is the Knuth Sequence */
-		interval = interval * 3 + 1;
-	}
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
 
-	while (interval > 0 && size > 1)
+	while (gap > 0)
 	{
-		for (i = interval; i < size; i++)
+		for (i = gap; i < size; i++)
 		{
-			for (n = i;
-			     n > interval - 1 && array[n - interval] > array[n];
-			     n -= interval)
-			{
-				swap_int(&array[n], &array[n - interval]);
-			}
+			insert = array[i];
+			for (j = i; j >= gap && array[j - gap] > insert; j = j - gap)
+				array[j] = array[j - gap];
+			array[j] = insert;
 		}
-		interval--;
-		interval = interval / 3;
+		gap = (gap - 1) / 3;
 		print_array(array, size);
 	}
-
-
 }
